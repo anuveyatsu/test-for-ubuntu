71f6048768b86035a5836faa69836dbb0c6d2a5f
anuveyatsu <anuar.ustayev@gmail.com>
2018-02-13 12:50:15 +0600

    [matrix][s]: trying to run different scripts for different OS

--- npm-yarn-installable.sh
+++ npm-yarn-installable.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -ev
-echo -ne '#######################################\nNow running shell script...'
+echo '>>> Now running shell script...'
 npm i -g data-cli
 data --version
 data help
 data info https://pkgstore.datahub.io/90998f7f90e086bd5fc7c9075dfda43b/basic-csv/comma-separated_csv/data/f4f7de54ccaaffa6ac0e5fe804653bf2/comma-separated_csv.csv
+
+echo '>>> Install data-cli with yarn...'
+npm uninstall -g data-cli
+npm i -g yarn
+yarn global add data-cli
+data --version
