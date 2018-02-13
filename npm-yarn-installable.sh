9542acb2cdcba758e75e70d878e3dd2e1d5cd5c6
anuveyatsu <anuar.ustayev@gmail.com>
2018-02-13 14:07:04 +0600

    [shell-script][xs]: removed 'sudo' when installing yarn on linux

--- npm-yarn-installable.sh
+++ npm-yarn-installable.sh
@@ -11,9 +11,9 @@ npm uninstall -g data-cli
 if [ $(uname) = 'Darwin' ]; then
   brew install yarn
 else
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  sudo apt-get update && sudo apt-get install yarn
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+  apt-get update && apt-get install yarn
 fi
 
 yarn global add data-cli
